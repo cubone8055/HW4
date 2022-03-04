5c9f8ffada75a84dc6ad3abce57ba6c8e11b0c80
cubone8055 <85774791+cubone8055@users.noreply.github.com>
2022-03-03 21:57:02 -0500

    Add files via upload

---
+++ HW4.R
@@ -0,0 +1,86 @@
+library(ggplot2)
+cars93 <- MASS::Cars93
+ggplot(cars93, aes(x = Price, y = Fuel.tank.capacity)) +
+  geom_point(color = "grey60") +
+  geom_smooth(se = TRUE, method = "lm", formula = y ~ x, color = "#8fe388") +
+  scale_x_continuous(
+    name = "price (USD)",
+    breaks = c(20, 40, 60),
+    labels = c("$20,000", "$40,000", "$60,000")
+  ) +
+  scale_y_continuous(name = "fuel-tank capacity\n(US gallons)") +
+  ggtitle("LM Method") +
+  theme(plot.title = element_text(color='#8fe388', size=14))
+
+ggplot(cars93, aes(x = Price, y = Fuel.tank.capacity)) +
+  geom_point(color = "grey60") +
+  geom_smooth(se = TRUE, method = "glm", formula = y ~ x, color = "#fe8d6d") +
+  scale_x_continuous(
+    name = "price (USD)",
+    breaks = c(20, 40, 60),
+    labels = c("$20,000", "$40,000", "$60,000")
+  ) +
+  scale_y_continuous(name = "fuel-tank capacity\n(US gallons)") +
+  ggtitle("GLM Method") +
+  theme(plot.title = element_text(color='#fe8d6d', size=14))
+
+ggplot(cars93, aes(x = Price, y = Fuel.tank.capacity)) +
+  geom_point(color = "grey60") +
+  geom_smooth(se = TRUE, method = "gam", formula = y ~ x, color = "#7c6bea") +
+  scale_x_continuous(
+    name = "price (USD)",
+    breaks = c(20, 40, 60),
+    labels = c("$20,000", "$40,000", "$60,000")
+  ) +
+  scale_y_continuous(name = "fuel-tank capacity\n(US gallons)") +
+  ggtitle("GAM Method") +
+  theme(plot.title = element_text(color='#7c6bea', size=14))
+
+
+# ============================================================================
+library(tidyverse)
+library(lubridate)
+
+load("C://Users//DETPRO//Downloads//preprint_growth.rda") #please change the path if needed
+#my path is different from normal because I am on a different computer :)
+head(preprint_growth)
+preprint_growth %>% filter(archive == "bioRxiv") %>%
+  filter(count > 0) -> biorxiv_growth
+preprints<-preprint_growth %>% filter(archive %in%
+          c("bioRxiv", "arXiv q-bio", "PeerJ Preprints")) %>%filter(count > 0) %>%
+  mutate(archive = factor(archive, levels = c("bioRxiv", "arXiv q-bio", "PeerJ Preprints")))
+preprints_final <- filter(preprints, date == ymd("2017-01-01"))
+ggplot(preprints) +
+  aes(date, count, color = archive, fill = archive) +
+  geom_line(size = 1) +
+  scale_y_continuous(
+    limits = c(0, 600), expand = c(0, 0),
+    name = "preprints / month",
+    sec.axis = dup_axis(
+      breaks = preprints_final$count,
+      labels = c("arXivq-bio", "PeerJPreprints", "bioRxiv"),
+      name = NULL)
+  ) +
+  scale_x_date(name = "year",
+               limits = c(min(biorxiv_growth$date), ymd("2017-01-01"))) +
+  scale_color_manual(values = c("#0072b2", "#D55E00", "#009e73"),
+                     name = NULL) +
+  theme(legend.position = "none")
+
+preprint_full <- filter(drop_na(preprint_growth), count > 0 & date > ymd("2004-01-01"))
+
+preprint_full <- filter(preprint_full, archive == "bioRxiv" | archive == "F1000Research")
+
+ggplot(preprint_full) +
+aes(date, count, color = archive, fill = archive) +
+  geom_line(size = 1) +
+  scale_y_continuous(
+    limits = c(0, 600), expand = c(0, 0),
+    name = "preprints / month"
+  ) +
+  scale_x_date(name = "year",
+               limits = c(ymd("2014-02-01"), ymd("2017-01-01"))) +
+  scale_color_manual(values = c("#7c6bea", "#fe8d6d"),
+                     name = NULL) +
+  theme(legend.position = "right") +
+  ggtitle("Preprint Counts")
